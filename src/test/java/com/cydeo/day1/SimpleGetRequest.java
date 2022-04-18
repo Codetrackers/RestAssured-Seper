073a84bd72a5bc08e5f1d6da14640522ecd41022
seper <sepehr.tabakh@gmail.com>
2022-04-18 18:50:25 -0400

    commit2

--- src/test/java/com/cydeo/day1/SimpleGetRequest.java
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -1,6 +1,22 @@
 package com.cydeo.day1;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
 public class SimpleGetRequest {
 
+@Test
+    public void test1(){
 
+    String url="http://3.84.108.13:8000/v3/api-docs";
+    //send a get request and save response inside the Response object
+    Response response = RestAssured.get(url);
+    //print response status code
+    System.out.println("response.statusCode() = " + response.statusCode());
+
+    //print response body
+    response.prettyPrint();
+
+}
 }
